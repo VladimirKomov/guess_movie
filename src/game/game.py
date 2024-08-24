c36a544879faeb3ca9f5606a9ffd3b923f0e94af
Elena Komova <e.komova01@gmail.com>
2024-08-24 08:02:11 +0300

    canched ignor

--- src/game/game.py
+++ src/game/game.py
@@ -1,11 +1,13 @@
 from src.database.database_connection import DatabaseConnection
 from src.database.database_manager import ChoosingFilm
+from src.game.hits import KeywordsHint
 
 
 class Game:
     # creating a game
-    def __init__(self, user, points, film):
+    def __init__(self, user, points):
         self.user = user
         self.points = points
         # getting a random movie for the game
         self.film = ChoosingFilm._get_random_movie() 
+
