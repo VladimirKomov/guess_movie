6f95aed72930135221f33af2c09dfd31a9fbd632
VladimirKomov <v.e.komov@gmail.com>
2024-08-24 14:18:18 +0300

    added game file

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ from src.administration.fill_base import *
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
 
-# Создание объекта UserManager
+# Creating a UserManager object
 user_manager = UserManager()
 
-# Пример проверки роли пользователя (предположим, 2 - администратор)
+# Example of user role verification (suppose 2 is an administrator)
 def is_admin():
     return 'role_id' in session and session['role_id'] == 2
 
@@ -26,13 +26,13 @@ def login():
         nick_or_email = request.form['nick_or_email']
         password = request.form['password']
 
-        # Авторизация пользователя через UserManager
+        # # User authorization via User Manager
         success, result = user_manager.authenticate(nick_or_email, password)
 
         if success:
             user = result
-            session['username'] = user[1]  # user[1] - это поле nick
-            session['role_id'] = user[5]  # user[5] - это поле role_id
+            session['username'] = user[1]  # user[1] - this is nick
+            session['role_id'] = user[5]  # user[5] - this is role_id
             flash('Login successful!', 'success')
             return redirect(url_for('game'))
         else:
@@ -49,7 +49,7 @@ def register():
         birthdate = request.form['birthdate']
         password = request.form['password']
 
-        # Регистрация пользователя через UserManager
+        # User registration via UserManager
         success, message = user_manager.register(nick, email, name, birthdate, password)
 
         if success:
@@ -67,20 +67,6 @@ def logout():
     flash('You have been logged out.', 'success')
     return redirect(url_for('login'))
 
-@app.route('/game')
-def game():
-    if 'username' not in session:
-        return redirect(url_for('login'))
-    
-    hints = {
-        'keywords': 'jkljjbjhvkjh',
-        'year': 'hshsj;kj;j;',
-        'actors': 'kjnlkjn;',
-        'image': 'ohgosjgshjoj',
-        'description': 'hlahlghakljghalkhl'
-    }
-
-    return render_template('index.html', hints=hints)
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
@@ -118,6 +104,40 @@ def fill_all_data():
     fill_all_data_films_by_page(page)
     flash(f'All data for page {page} filled successfully.', 'success')
     return redirect(url_for('admin'))
+
+# @app.route('/game')
+# def game():
+#     if 'username' not in session:
+#         return redirect(url_for('login'))
+    
+#     hints = {
+#         'keywords': 'jkljjbjhvkjh',
+#         'year': 'hshsj;kj;j;',
+#         'actors': 'kjnlkjn;',
+#         'image': 'ohgosjgshjoj',
+#         'description': 'hlahlghakljghalkhl'
+#     }
+
+#     return render_template('index.html', hints=hints)
+
+@app.route('/start_game', methods=['POST'])
+def start_game():
+@app.route('/start_game', methods=['POST'])
+def start_game():
+    # Здесь вы должны сгенерировать или выбрать фильм для игры
+    movie = get_random_movie()  # Предположим, у вас есть функция, выбирающая случайный фильм
+    hints = {
+        'keywords': KeywordsHint().get_hint(movie),
+        'year': YearHint().get_hint(movie),
+        'actors': ActorsHint().get_hint(movie),
+        'image': ImageHint().get_hint(movie),
+        'description': DescriptionHint().get_hint(movie)
+    }
+    session['movie_id'] = movie.id  # Сохраняем идентификатор фильма в сессии
+    session['hints'] = hints  # Сохраняем подсказки в сессии
+    return render_template('index.html', hints=hints, game_started=True)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
